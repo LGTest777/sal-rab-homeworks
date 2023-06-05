75d84d87213af4ea79e3e58ef4b6e80e4b15ca99
LG <LGtest@gmail.com>
2023-06-05 07:59:38 +0300

    Final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {
+        client: name + "" + phone, 
+        order: {
+            address: "", 
+            sum: ""
+        }, 
+        goods: [
+           {title: "",
+            count: ""
+        } 
+        ]
+    };
 
     let countOfGoods = goods.length;
 
@@ -44,7 +55,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
-
+    
     data.client = 'Иван';
 
     let jsonData = JSON.stringify(data);
